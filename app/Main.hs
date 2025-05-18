c264341b61f82232135136bf172a1dd8918eda15
Petr Laštovička <lastope2@fit.cvut.cz>
2025-05-18 17:24:29 +0300

    build: Build setup

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- | Main entry point for the Debt Diary application
+module Main where
+
+-- | Main function that processes command line arguments and executes the appropriate command
+main :: IO ()
+main = do
+  putStrLn "Hello world"
